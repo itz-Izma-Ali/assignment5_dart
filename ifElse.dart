2531ef1f199446c0b4a59eb2c020a08047122afa
itz-Izma-Ali <izma91700@gmail.com>
2024-09-22 23:02:12 +0500

    Positive, Negative, or Zero Checker (If-Else)

---
+++ ifElse.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = -3; 
+
+  if (number > 0) {
+    print("The number is positive.");
+  } else if (number < 0) {
+    print("The number is negative.");
+  } else {
+    print("The number is zero.");
+  }
+}
